d455f4b2e78914697523bb92383d8423bddd61dc
Jytia Wright <jytia.wright@greenschoolbrooklyn.com>
2017-02-14 14:46:36 +0000

    Turtle directions

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-TinyTurtle.apply(window); 
+var turtle = new TinyTurtle();
+
+//turtle.forward(30);
+
+function square(){
+ turtle.forward(30);
+ turtle.right(30);
+ turtle.reverse(30);
+ turtle.left(30);
+}
+
+//turtle.right(30);
+  
+//turtle.left(30);
+  
+//turtle.reverse(30);
